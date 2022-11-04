1a8c1eb7d44532eda05def0e2a5324acb9aee0ec
DOLGIY-DMITRIY <dolgiy.dmitriyp@gmail.com>
2022-11-04 16:36:08 +0100

    Added avoiding collisions with the same voice name

--- src/main/kotlin/com/voicybot/io/statemachine/applier/CallVoice.kt
+++ src/main/kotlin/com/voicybot/io/statemachine/applier/CallVoice.kt
@@ -6,10 +6,21 @@ import com.github.kotlintelegrambot.entities.Message
 import com.voicybot.io.statemachine.ExecutionOutput
 import com.voicybot.io.statemachine.state.State
 
-class CallVoice: Applier {
+class CallVoice : Applier {
+
+    private var names = mutableListOf<String>()
+
     override fun apply(bot: Bot, message: Message): ExecutionOutput? {
-        if(message.text != null){
+        if (message.text != null) {
+
+            if (names.contains(message.text.toString())) {
+                bot.sendMessage(ChatId.fromId(message.chat.id),
+                    "Sorry, but you already have a voice with the same name..." +
+                            " Try another name")
+                return ExecutionOutput(State.GET_VOICE, "")
+            }
             bot.sendMessage(ChatId.fromId(message.chat.id), "Now your sticker has name : ${message.text.toString()}")
+            names.add(message.text.toString())
             return ExecutionOutput(State.CALL_VOICE, message.text.toString())
         }
 
