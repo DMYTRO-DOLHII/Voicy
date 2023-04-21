adee580905796e0940a62c11f51a39875c2425f7
DOLGIY-DMITRIY <dolgiy.dmitriyp@gmail.com>
2023-04-21 10:08:13 +0200

    Sound extraction from Tiktok (video, images, audio)

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,13 @@ repositories {
 dependencies {
     // Web Scraping
     implementation("org.jsoup:jsoup:1.14.1")
+
+    // FFmpeg converter
     implementation("org.bytedeco:javacv-platform:1.5.6")
+
+    // JSON
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.+")
+
     testImplementation(kotlin("test"))
     implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.0.7")
     implementation("com.google.firebase:firebase-admin:9.1.0")
--- src/main/kotlin/com/voicybot/io/bot/web/TikTok.kt
+++ src/main/kotlin/com/voicybot/io/bot/web/TikTok.kt
@@ -1,9 +1,10 @@
 package com.voicybot.io.bot.web
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.jsoup.Jsoup
 import java.io.InputStream
 import java.net.CookieManager
-import java.util.regex.Pattern
+import java.util.*
 
 object TikTok {
 
@@ -13,25 +14,69 @@ object TikTok {
             .cookieStore(cookieStore)
             .get()
 
-        val pattern = Pattern.compile("\"playAddr\":\"(.*?)\"")
-        val matcher = pattern.matcher(document.toString())
+        val foundTags = document.body().select("#SIGI_STATE")
+        val scriptTag = foundTags.single()
+        val json = scriptTag.data()
 
-        if (matcher.find()) {
-            var videoUrl = matcher.group(1)
-            videoUrl = videoUrl.replace("\\u002F", "/")
+        val videoUrl = extractVideoUrl(json).orElseGet { extractSoundUrl(json).get() }
 
+        return Jsoup.connect(videoUrl)
+            .referrer("https://www.tiktok.com/")
+            .cookieStore(cookieStore)
+            // Jsoup expects text types only and throws exception if receives any other types.
+            .ignoreContentType(true)
+            .execute()
+            .bodyStream()
 
-            return Jsoup.connect(videoUrl)
-                .referrer("https://www.tiktok.com/")
-                .cookieStore(cookieStore)
-                // Jsoup expects text types only and throws exception if receives any other types.
-                .ignoreContentType(true)
-                .execute()
-                .bodyStream()
+    }
 
+    private fun extractVideoUrl(json: String): Optional<String> {
+        val objectMapper = jacksonObjectMapper()
+        val jsonNode = objectMapper.readTree(json)
+        val directVideoUrl: String
+        try {
+            val videoId = jsonNode
+                .get("ItemList")
+                .get("video")
+                .get("keyword")
+                .asText()
+            directVideoUrl = jsonNode.get("ItemModule")
+                .get(videoId)
+                .get("music")
+                .get("playUrl")
+                .asText()
+        } catch (e: NullPointerException) {
+            return Optional.empty()
         }
 
-        println("Error appeared while something...")
-        return InputStream.nullInputStream()
+        if (directVideoUrl.isNotEmpty()){
+            return Optional.of(directVideoUrl)
+        }
+        return Optional.empty()
+    }
+
+    private fun extractSoundUrl(json: String): Optional<String> {
+        val objectMapper = jacksonObjectMapper()
+        val jsonNode = objectMapper.readTree(json)
+        val directSoundUrl: String
+        try {
+            val itemId = jsonNode
+                .get("ItemList")
+                .get("music")
+                .get("list")
+                .get(0)
+                .asText()
+            directSoundUrl = jsonNode.get("ItemModule")
+                .get(itemId)
+                .get("music")
+                .get("playUrl")
+                .asText()
+        } catch (e: NullPointerException) {
+            return Optional.empty()
+        }
+        if (directSoundUrl.isNotEmpty()) {
+            return Optional.of(directSoundUrl)
+        }
+        return Optional.empty()
     }
 }
